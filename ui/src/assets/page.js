9c2c8f5536fe0f2d1afe69d6f3f44481c49c65e4
Araj P Raju <arajparaj@gmail.com>
2016-10-12 02:32:33 +0530

    dynamic navigation

--- ui/src/assets/page.js
+++ ui/src/assets/page.js
@@ -1,12 +1,38 @@
-var page = require('page');
+import page from 'page';
+
 $(document).ready(function() {
+    let bodyContainer = $('#container-body');
+
     page('/', function() {
-        console.log('indexpage')
+        // bodyContainer.load('/pages/mortgage/buy/residential/home.html');
+        page.redirect('/mortgage');
     });
 
-    page('/hash', function() {
-        console.log('hash page')
+    page('/mortgage', function() {
+        $('.dropdown-menu a').each(function() {
+            this.href = this.baseURI + this.pathname;
+        });
     });
+
+    page('/mortgage/buy/residential', function() {
+        bodyContainer.load('/pages/mortgage/buy/residential/home.html');
+    });
+
+    page('/mortgage/buy/commercial', function() {
+        bodyContainer.load('/pages/mortgage/buy/commercial/home.html');
+    });
+
+    page('/mortgage/sell/residential', function() {
+        bodyContainer.load('/pages/mortgage/sell/residential/home.html');
+    });
+
+    page('/mortgage/sell/commercial', function() {
+        bodyContainer.load('/pages/mortgage/sell/commercial/home.html');
+    });
+
+    page('*', function() {
+        page.redirect('/mortgage');
+    })
 
     page();
 });

f230a15f4a9de52118f708782841f7df4262c930
Araj P Raju <arajparaj@gmail.com>
2016-10-11 13:44:16 +0530

    proper mobile

--- ui/src/assets/page.js
+++ ui/src/assets/page.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.list-inline , .button-container, .bottom-navbar').hover(function() {
+    $('.navbar-nav , .button-container, .bottom-navbar').hover(function() {
         $('.overlay').fadeIn();
     }, function() {
         $('.overlay').fadeOut();
